1a9ef013404368ec340b41bf3323cb1bb494bc25
Marco Ridder <info@marcoridder.nl>
2021-11-25 19:23:56 +0100

    Install.sh apt update -y

--- tools/install.sh
+++ tools/install.sh
@@ -6,7 +6,7 @@ echo '################################'
 
 sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-sudo apt update
+sudo apt update -y
 sudo apt install -y vim apache2 php8.0 php8.0-xml php8.0-mbstring php8.0-curl php8.0-zip
 sudo a2enmod rewrite
 
